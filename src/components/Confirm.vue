2d81129ab06d546f8560178c68fd4a2c27aca54a
Teodor Angelov <teodor.angelov@fastcomet.com>
2024-02-09 17:12:51 +0200

    style: confirm modal rework

--- src/components/Confirm.vue
+++ src/components/Confirm.vue
@@ -1,24 +1,38 @@
-<script>
-
+<script setup>
+import Svg from '@/assets/svg/index.js'
 </script>
 
 <template>
     <div class="success-modal">
-        <svg viewBox="0 0 114 114" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="57" cy="57" r="55.5" fill="#5A9D5D" stroke="white" stroke-width="3"/>
-            <g clip-path="url(#clip0_438_16)">
-            <path d="M67.6524 75.2911C65.2764 79.3885 62.3961 83.17 59.0792 86.5466C54.8998 90.7092 50.9204 91.2466 46.6696 87.392C38.2393 79.7474 30.2377 71.5512 22.5504 63.1686C19.457 59.8299 21.2716 54.8147 25.6725 51.8773C28.7231 49.8354 31.1593 50.8312 34.36 54.6285C38.7394 59.8013 43.4261 64.7162 47.9698 69.7457C50.2917 72.3178 52.6208 71.8951 54.6069 69.5021C56.3444 67.3922 57.898 65.1366 59.2507 62.7602C65.9735 50.9745 72.5891 39.1172 79.2904 27.3243C80.4264 25.3254 81.7909 23.3981 84.6201 24.1719C87.7707 25.0388 92.0287 29.4378 92.0073 32.361C91.6143 33.1276 91.0285 34.41 90.3355 35.628" fill="white"/>
-            </g>
-            <defs>
-            <clipPath id="clip0_438_16">
-            <rect width="71" height="66" fill="white" transform="translate(21 24)"/>
-            </clipPath>
-            </defs>
-        </svg>
+        <div class="modal-body py-4">
+            <div v-html="Svg.Confirm"></div>
+            <h3 class="text-center mb-4">Вашата резервация е успешна!</h3>
+            <button class="reserve">НАПРЕД</button>
+        </div>
     </div>
 </template>
   
-<style lang="css" scoped>
+<style lang="scss" scoped>
+    .reserve {
+        border: none;
+        background-color: #E08D41;
+        color: #FFF6E5;
+        border-radius: 30px;
+        font-size: 1.2rem;
+        font-weight: 600;
+        width: fit-content;
+        padding: 5px 20px;
+        margin: auto;
+        display: block;
+        &:active {
+            background-color: darken(#E08D41, 8%);
+            color: #F3E0B8;
+            scale: 1.03;
+        }
+    }
+</style>
+
+<style lang="scss">
     .success-modal {
         position: absolute;
         top: 0;
@@ -29,8 +43,19 @@
         display: flex;
         align-items: center;
         justify-content: center;
-    }
-    svg {
-        width: 163px;
+        .modal-body {
+            background-color: #333;
+            margin: 50px;
+            padding: 10px;
+            border-radius: 6px;
+            div {
+                width: 120px;
+                margin-inline: auto;
+                margin-bottom: 8px;
+                svg {
+                    width: 100%;
+                }
+            }
+        }
     }
 </style>
