98e586c094f62c7a63758dff95b8eca4776a50fc
Teodor Angelov <jokov.unknown@gmail.com>
2024-02-25 01:39:59 +0200

    style: add splashscreen for most of the apple devices

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path';
 import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
-  server: { https: true },
+  server: { https: false },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
